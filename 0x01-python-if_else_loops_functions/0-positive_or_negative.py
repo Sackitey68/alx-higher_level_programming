32a724831343f898a0bf056fb8f77dc33d0ccdba
root <sackitey68@gmail.com>
2023-06-06 10:15:35 -0700

    fixed whitespace error on task 0

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,7 +4,7 @@ number = random.randint(-10, 10)
 if (number > 0):
     print("{} is positive".format(number))
 
-elif(number == 0):
+elif (number == 0):
     print("{} is zero".format(number))
 
 else:
