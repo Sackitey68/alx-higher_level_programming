fde964b68f792f76c263e1671067637822145b5d
root <sackitey68@gmail.com>
2023-07-11 17:24:00 -0700

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a string-to-JSON function"""
-import json
+"""This module defines a file-appending function."""
 
 
-def to_json_string(my_obj):
-    """Returns the JSON representation of a string object"""
-    return json.dumps(my_obj)
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
