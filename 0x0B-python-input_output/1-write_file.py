62a9ec66cca74c50f8103b567bc494dac51ed3e4
root <sackitey68@gmail.com>
2023-07-11 17:19:45 -0700

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""
-Program that counts lines in a file
-"""
+"""This module defines a file-writing function."""
 
 
-def number_of_lines(filename=""):
-    """ count lines in file """
-    count = 0
-    if filename == "":
-        return count
-    with open(filename, "r") as f:
-        for l in f:
-            count += 1
-    return count
+def write_file(filename="", text=""):
+    """Writes a string to a UTF8 text file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
