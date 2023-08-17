2b85f13bbda1471449d6c01e04d0f439f102258d
root <sackitey68@gmail.com>
2023-08-16 21:36:11 -0700

    Task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Script that lists all the cities of California registered in the database
+SELECT id, name
+FROM cities
+WHERE state_id = (
+      SELECT id
+      FROM states
+      WHERE name = "California");
