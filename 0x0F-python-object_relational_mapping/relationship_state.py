5d18250a0944d670e2956113d36c78c23082794a
root <sackitey68@gmail.com>
2023-09-23 23:26:55 -0700

    Added relationship_state

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+"""
+Explaines a State model
+Inherits from SQLAlchemy Base and links to the MySQL table states
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
@@ -10,7 +12,6 @@ from relationship_city import Base, City
 
 class State(Base):
     """Represents a state for a MySQL database.
-
     Attributes:
         __tablename__ (str): The name of the MySQL table to store States.
         id (sqlalchemy.Integer): The state's id.
