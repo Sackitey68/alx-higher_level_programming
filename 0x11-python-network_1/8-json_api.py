b81a33e43199befaaf1042b5294465863d8a8c17
root <sackitey68@gmail.com>
2023-09-29 19:38:14 -0700

    Task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" Use requests to make a post and return body """
-import requests
-from sys import argv
-
-
-if __name__ == "__main__":
-    if len(argv) < 2:
-        data = {"q": ""}
+"""
+Accepts a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    if len(argv) == 2:
+        q = argv[1]
     else:
-        data = {"q": argv[1]}
-    url = "http://0.0.0.0:5000/search_user"
-    res = requests.post(url, data)
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
-        _json = res.json()
-    except ValueError:
+        r_dict = r.json()
+        id = r_dict.get('id')
+        name = r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except Exception:
         print("Not a valid JSON")
-    if len(_json) < 1 or ("id" and "name") not in _json:
-        print("No result")
-    else:
-        print("[{}] {}".format(_json["id"], _json["name"]))
