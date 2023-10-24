4aeecd3d37222405b7c07a00bae230d611aef036
root <sackitey68@gmail.com>
2023-10-24 11:03:10 -0700

    Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,28 +1,16 @@
 #!/usr/bin/node
 
 const request = require('request');
-const argv = process.argv;
-const url = 'http://swapi.co/api/films/';
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-function getStatusJson (theUrl) {
-  const options = {
-    url: theUrl,
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Accept-Charset': 'utf-8'
-    }
-  };
-  request(options, function (err, res, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      const json = JSON.parse(body);
-      const status = res.statusCode;
-      console.log(json.title);
-      return (status, json);
-    }
-  });
-}
-
-getStatusJson(url + argv[2]);
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
