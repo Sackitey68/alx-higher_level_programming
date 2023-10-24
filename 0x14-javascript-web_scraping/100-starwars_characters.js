1dfa1e3f2fa44e63bef802c9d20189327fb9bf20
root <sackitey68@gmail.com>
2023-10-24 10:28:15 -0700

    Task 7

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,52 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+const request = require('request');
+const argv = process.argv;
+const url = 'http://swapi.co/api/films/' + argv[2];
+
+function getcharacter (theUrl) {
+  const options = {
+    url: theUrl,
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Accept-Charset': 'utf-8'
     }
-  }
-});
+  };
+  request(options, function (err, res, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      const json = JSON.parse(body);
+      // const status = res.statusCode;
+      // console.log(json);
+      console.log(json.name);
+    }
+  });
+}
+
+function getJson (theUrl) {
+  const options = {
+    url: theUrl,
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Accept-Charset': 'utf-8'
+    }
+  };
+  request(options, function (err, res, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      const json = JSON.parse(body);
+      // const status = res.statusCode;
+      // console.log(json);
+      const characters = json.characters;
+      for (const i in characters) {
+        getcharacter(characters[i]);
+      }
+      return json;
+    }
+  });
+}
+getJson(url);
